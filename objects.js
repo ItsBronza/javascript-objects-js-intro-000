005aa655315e623a0c72ce007506c06ea38edfd3
Alyona <itsbronza@yahoo.com>
2019-11-06 11:18:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,33 +1,33 @@
-var playlist = {} 
-playlist.Beyonce = 'fear' 
-playlist['BTS'] = 'fire' 
+var playlist = {}
+playlist.Beyonce = 'fear'
+playlist['BTS'] = 'fire'
 
- 
 
-function updatePlaylist(playlist, artist, song) { 
-  playlist[artist]= song 
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist]= song
   return playlist
-  
-  
+
+
 }
 
 updatePlaylist(playlist,'Taylor', 'red')
 console.log(playlist)
 
- 
 
-function removeFromPlaylist(playlist, artist) { 
+
+function removeFromPlaylist(playlist, artist) {
   delete playlist[artist]
-  return playlist 
-  
-  
+  return playlist
+
+
 }
 removeFromPlaylist(playlist,'Taylor')
- 
- var recipe = {flour: '1 cup', eggs: 2, sugar: '4 cups'} 
- delete recipe.eggs 
- recipe 
- 
- var sugar = 'sugar' 
+
+ var recipe = {flour: '1 cup', eggs: 2, sugar: '4 cups'}
+ delete recipe.eggs
+ recipe
+
+ var sugar = 'sugar'
  delete recipe[sugar]
  recipe 
