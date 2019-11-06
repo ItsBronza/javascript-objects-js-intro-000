293fe9b59ced09cc824dff54d46d95fbdaef1873
Alyona <itsbronza@yahoo.com>
2019-11-06 10:32:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,1 @@
-var playlist = {Linkin Park : "Numb",
-                Limp Bizkit : "Behind Blue Eyes",
-                Evanescence : "Fallen"
-              }
+var playlist = {Linkin Park : "Numb"}
